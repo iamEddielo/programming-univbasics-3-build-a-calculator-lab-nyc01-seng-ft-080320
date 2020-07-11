2bd8936b894db83741549037586e76de212278ea
Eddie Lopez <iameddielopez@gmail.com>
2020-07-11 16:52:02 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,29 +1,35 @@
 def addition(num1, num2)
   sum = num2 + num1
   puts sum
+  sum
 end
 
 def subtraction(num1, num2)
   sub = num2 - num1
   puts sub
+  sub
 end
 
 def division(num1, num2)
   div = num2 / num1
   puts div
+  div
 end
 
 def multiplication(num1, num2)
   mult = num1 * num2
   puts mult
+  mult
 end
 
 def modulo(num1, num2)
   mod = num2 % num1
   puts mod
+  mod
 end
 
 def square_root(num)
   square = num.sqrt
   puts square
+  square
 end
